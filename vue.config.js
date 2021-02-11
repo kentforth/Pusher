397824e74850e54189b7c937a6564ece3ec321eb
kentforth <yashaar@mail.ru>
2021-02-11 22:23:35 +0700

    change webpack config

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,13 @@ module.exports = {
         prependData: `@import "@/assets/scss/styles.scss";`
       }
     }
+  },
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000
+      }
+    }
   }
 };
